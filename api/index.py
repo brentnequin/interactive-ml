b76dc33dfa2d4785f627aaf6eb5c2bf056f48505
brentnequin <brentnequin@gmail.com>
2023-01-17 01:32:03 -0600

    kmeans

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, request
+from flask_restful import Resource, Api
+import numpy as np
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/api')
-def home():
-    return 'Hello, World!'
+class KMeans(Resource):
+    def post(self, ):
+        body = request.json
+        
+        print(body)
+        
+        return None # response
+    
+api.add_resource(KMeans, '/api/execute/kmeans')
 
-@app.route('/api/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
