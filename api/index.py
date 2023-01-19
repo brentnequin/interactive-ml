3e6325dbad19c1061431d29ccd48df6f32b9b424
brentnequin <brentnequin@gmail.com>
2023-01-19 00:49:32 -0600

    cwaft

--- api/index.py
+++ api/index.py
@@ -4,7 +4,11 @@ from flask_restful import Resource, Api
 import numpy as np
 import json
 
-from sklearn import cluster
+from cwaft.algorithms import kmeans
+
+from util.json import NpEncoder
+
+# from sklearn import cluster
 # from sklearn_extra.cluster import KMedoids
 
 app = Flask(__name__)
@@ -21,22 +25,17 @@ class KMeans(Resource):
         try:
 
             X = np.array([(point['x'], point['y']) for point in data['points']])
-            kmeans = cluster.KMeans(n_clusters=data.get('k'), random_state=0, n_init="auto").fit(X)
-
-            cluster_centers = [{ 'x': float(x), 'y': float(y), 'label': index} for index, (x, y) in enumerate(list(map(tuple, kmeans.cluster_centers_)))]
-            points_labeled = [{ 'x': point['x'], 'y': point['y'], 'cluster_center': int(label) } for point, label in zip(data['points'], kmeans.labels_)]
-
-            response_data = {
-                'k': data['k'],
-                'cluster_centers': cluster_centers,
-                'points': points_labeled
+            model = kmeans.KMeans()
+            model.fit(X)
+            result = {
+                "centeroids": [dict(x=x, y=y) for [x, y] in model.centeroids],
+                "points": [dict(x=x, y=y, centeroid=label) for [x, y, label] in np.column_stack([X, model.labels])]
             }
-
-            response = Response(json.dumps(response_data), mimetype='application/json')
+            response = Response(json.dumps(result, cls=NpEncoder), mimetype='application/json')
 
         except KeyError as e:
 
-            response = Response(json.dumps({ 'message': f"Missing data in payload: {str(e)}" }), status=500, mimetype='application/json')
+            response = Response(json.dumps({ 'message': f"Invalid data in payload: {str(e)}" }), status=500, mimetype='application/json')
         
         return response
     
--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,27 @@ import Plotly from 'plotly.js-dist'
 
 const plot = ref<HTMLDivElement>()
 
+let points = [
+  { x: 1, y: 1 },
+  { x: 3, y: 3 },
+  { x: 5, y: 5 },
+  { x: 7, y: 7 },
+]
+
 onMounted(()=>{    
+  refreshPlot()
+})
+
+function refreshPlot() {
   Plotly.newPlot(plot.value, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16],
+    x: points.map((({x}) => x)),
+    y: points.map((({y}) => y)),
     mode: 'markers',
     type: 'scatter'
   }], {
     margin: { t: 0 }
   });
-})
+}
 
 function addPoint(event) {
   var { left, top } = plot.value.getBoundingClientRect();
@@ -21,10 +32,11 @@ function addPoint(event) {
 	var xInDataCoord = plot.value._fullLayout.xaxis.p2c(event.x - plot.value._fullLayout.margin.l - left + document.documentElement.scrollLeft);
 	var yInDataCoord = plot.value._fullLayout.yaxis.p2c(event.y - plot.value._fullLayout.margin.t - top + document.documentElement.scrollTop);
 
-  plot.value.data[0].x.push(xInDataCoord)
-  plot.value.data[0].y.push(yInDataCoord)
+  // plot.value.data[0].x.push(xInDataCoord)
+  // plot.value.data[0].y.push(yInDataCoord)
 
-  Plotly.redraw(plot.value);
+  points.push({ x: xInDataCoord, y: yInDataCoord })
+  refreshPlot()
 }
 
 async function run() {
