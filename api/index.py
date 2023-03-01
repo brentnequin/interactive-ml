9c7ba6ba8755ab2d423db4e61bd5799280e7ebfb
Brent Nequin <brentnequin@gmail.com>
2023-02-28 21:14:43 -0600

    hmmmmmm

--- api/index.py
+++ api/index.py
@@ -1,4 +1,23 @@
-from src.api.index import app
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+
+import os
+from dotenv import load_dotenv
+
+from src.api.resources import APIStatus, KMeans
+
+dotenv_path = os.path.join(os.getcwd(), '.env.local')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+api.add_resource(APIStatus, '/api')
+api.add_resource(KMeans, '/api/execute/kmeans')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
